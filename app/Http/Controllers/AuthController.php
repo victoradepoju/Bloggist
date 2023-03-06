34d8ba4848e45312bacadd675490926717062fff
victoradepoju <victoradepoju30@gmail.com>
2023-03-07 00:08:04 +0100

    register, login, logout controllers

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+// use App\Http\User;
 
 class AuthController extends Controller
 {
-    //
+    // Register user
+    public function register(Request $request)
+    {
+        // validate fields
+        $attrs = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        // create user
+        $user = User::create([
+            'name' => $attrs['name'],
+            'email' => $attrs['email'],
+            'password' => bcrypt($attrs['password']),
+        ]);
+
+        // return user & token in response
+        return response([
+            'user' => $user,
+            'token' => $user->createToken('secret')->plainTextToken,
+        ]);
+    }
+
+    // login user
+    public function login(Request $request)
+    {
+        // validate fields
+        $attrs = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // attempt login
+        if (!Auth::attempt($attrs)) {
+            return response([
+                'message' => 'Invalid credentials',
+            ], 403);
+        }
+
+        // return user & token in response
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken,
+        ], 200);
+    }
+
+    // logout user
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout success',
+        ], 200);
+    }
 }
