f96ceb4cb58c14961baefd3b01c19ca4d8fcd6ba
victoradepoju <victoradepoju30@gmail.com>
2023-03-07 00:59:06 +0100

    post, comment & like models/controllers

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Like;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+        'image',
+    ];
+}
